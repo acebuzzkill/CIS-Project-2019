25c365e27165581bbab3b772bab84c029752e542
Hotsuye <49544183+Hotsuye@users.noreply.github.com>
2019-09-24 18:23:13 -0700

    No Change (Updating Version Control)

---
+++ ClaremontConnection/app/src/main/java/com/example/claremountconnection/FindAlumni.java
@@ -0,0 +1,18 @@
+package com.example.claremountconnection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindAlumni extends BaseActivityToolbar {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public int getLayoutResource() {
+        return R.layout.activity_find_alumni;
+    }
+}
---
+++ ClaremontConnection/app/src/main/java/com/example/claremountconnection/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.claremountconnection;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends BaseActivityToolbar {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public int getLayoutResource() {
+        return R.layout.activity_main;
+    }
+}
+
+
