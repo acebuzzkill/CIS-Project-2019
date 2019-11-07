64f4346ea6ca0dc7f63fcfa4bcad589caff70c77
Hotsuye <49544183+Hotsuye@users.noreply.github.com>
2019-11-06 16:35:37 -0800

    Trying To Sync My Shit

--- ClaremontConnection/app/src/main/java/com/example/claremountconnection/FindAlumni/FindAlumni.java
+++ ClaremontConnection/app/src/main/java/com/example/claremountconnection/FindAlumni/FindAlumni.java
@@ -1,18 +1,49 @@
 package com.example.claremountconnection.FindAlumni;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.claremountconnection.BaseActivityToolbar;
 import com.example.claremountconnection.R;
 
+import java.util.ArrayList;
+
 public class FindAlumni extends BaseActivityToolbar {
+
+    SearchView alumniSearch;
+    ListView alumniList;
+
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        alumniSearch = (SearchView)findViewById(R.id.search_view_alumni);
+        alumniList = (ListView)findViewById(R.id.search_list_results);
+
+        list.add("Bob");
+        list.add("Bobby");
+        list.add("Boblina");
+        list.add("Bobby-joe");
+        list.add("Bobsan");
+        list.add("Bobbydo");
+        list.add("Bobbydon't");
+        list.add("Bobbie");
+
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
+
+        alumniList.setAdapter(adapter);
+
+        alumniSearch.setOnQueryTextListener(new SearchView.OnQueryTextListner() );
     }
+
+
 
     @Override
     public int getLayoutResource() {
---
+++ ClaremontConnection/app/src/main/java/com/example/claremountconnection/Messages/MessageOpened.java
@@ -0,0 +1,4 @@
+package com.example.claremountconnection.Messages;
+
+public class MessageOpened {
+}
