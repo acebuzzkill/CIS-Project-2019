ba5141501dc01784dc5b6b86b9230a7b4af844e1
ntmach <ntmach@cpp.edu>
2019-11-22 01:00:20 -0800

    First Combine

--- ClaremontConnection/app/src/main/java/com/example/claremountconnection/FindAlumni.java
+++ ClaremontConnection/app/src/main/java/com/example/claremountconnection/FindAlumni.java
@@ -1,11 +1,19 @@
 package com.example.claremountconnection;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.claremountconnection.BaseActivityToolbar;
+import com.example.claremountconnection.R;
+
+import java.util.ArrayList;
 
 public class FindAlumni extends BaseActivityToolbar {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ ClaremontConnection/app/src/main/java/com/example/claremountconnection/Messages/MessageAdapter.java
@@ -0,0 +1,92 @@
+package com.example.claremountconnection.Messages;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.claremountconnection.R;
+
+import java.util.ArrayList;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ExampleViewHolder> {
+    private ArrayList<MessageGetImageText> mExampleList;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+
+        void onDeleteClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+        public ImageView mDeleteImage;
+
+        public ExampleViewHolder(View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+            mDeleteImage = itemView.findViewById(R.id.image_delete);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+
+            mDeleteImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onDeleteClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public MessageAdapter(ArrayList<MessageGetImageText> exampleList) {
+        mExampleList = exampleList;
+    }
+
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.example_hard_code_message_text, parent, false);
+        ExampleViewHolder evh = new ExampleViewHolder(v, mListener);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, int position) {
+        MessageGetImageText currentItem = mExampleList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mTextView1.setText(currentItem.getText1());
+        holder.mTextView2.setText(currentItem.getText2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();
+    }
+}
